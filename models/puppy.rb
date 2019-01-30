f7032d64a524aa278bc7532fb698f224c95410b4
Sean Langton <slangton1@gmail.com>
2019-01-30 22:03:22 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
 class Puppy
 
-  attr_accessor :name, :breed, :months_old
+  attr_accessor :name, :breed, :age
 
   def initialize(name:, breed:, months_old:)
     @name = name
     @breed = breed
-    @months_old = months_old
+    @age = months_old
   end
 
 end
